29c1e72fcefe4bab1aac36f20a56c533d6df25d3
wiusxt <ashatovanas65@gmail.com>
2022-04-11 15:30:53 +0600

    App.js file was update

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
 
-import Footer from "./components/Footer/Footer";
+
 import Header from "./components/Header/Header";
 import Nav from "./components/Nav/Nav";
+import Footer from "./components/Footer/Footer";
+
+import Home from "./pages/Home";
+import Contacts from "./pages/Contacts";
+import Products from "./pages/Products";
 
 function App() {
   return (
     <div className="App">
       <Nav />
       <Header />
+
+      <Home />
+      <Contacts />
+      <Products />
+
       <Footer />
     </div>
-  );
+  )
 };
 
 export default App;
-
