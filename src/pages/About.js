16a680d4395e35f8669d290bda118a7aeae945e2
wiusxt <ashatovanas65@gmail.com>
2022-04-13 15:50:19 +0600

    added className in article

--- src/pages/About.js
+++ src/pages/About.js
@@ -2,8 +2,8 @@ import "./About.css"
 import React from "react";
 function About() {
     return (
-      <article>
-        <h1 className="about-page">This page about us and our company.</h1>
+      <article className="about-page">
+        <h1 >This page about us and our company.</h1>
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ex pariatur voluptatum doloribus enim repudiandae ullam sequi quo suscipit voluptas id, laboriosam necessitatibus, dolore corrupti. Explicabo iure doloribus illum repudiandae tenetur.</p>
       </article>
     );
