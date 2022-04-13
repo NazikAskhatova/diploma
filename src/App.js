b7069106c171f0a98302339a55e5eb219d60e5e0
wiusxt <ashatovanas65@gmail.com>
2022-04-13 14:33:01 +0600

    installed router react

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import Footer from "./components/Footer/Footer";
 import Home from "./pages/Home";
 import Contacts from "./pages/Contacts";
 import Products from "./pages/Products";
+import About from "./pages/About";
+import Shop from "./pages/Shop";
 
 function App() {
   return (
@@ -17,6 +19,8 @@ function App() {
       <Home />
       <Contacts />
       <Products />
+<About />
+<Shop />
 
       <Footer />
     </div>
