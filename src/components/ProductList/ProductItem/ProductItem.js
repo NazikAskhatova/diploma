66d36fdbe7ad1c61a470b264954a2108f4b50094
wiusxt <ashatovanas65@gmail.com>
2022-04-20 15:39:21 +0600

    added images in assets

--- src/components/ProductList/ProductItem/ProductItem.js
+++ src/components/ProductList/ProductItem/ProductItem.js
@@ -1,9 +1,15 @@
-import classes from "./ProductItem.module.css"
+import classes from "./ProductItem.module.css";
+import { Link } from "react-router-dom";
+import productImage from "../../../assets/products-6.jpg";
 
 function ProductItem({ product }) {
     return (
         <div className={classes.ProuctItem}>
-
+<img src={productImage}/>
+<div>
+    <Link to="/">Rings</Link>
+ <div className={classes.price}>$79.9</div>
+</div>
         </div>
     )
 }
