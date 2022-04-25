57eef5965d78f8665f3c98a4c94fb757b674f32b
wiusxt <ashatovanas65@gmail.com>
2022-04-25 14:43:37 +0600

    app.js and index.js was updated

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import Header from "./components/Header/Header";
-import Nav from "./components/Nav/Nav";
-import Footer from "./components/Footer/Footer";
+// import Header from "./components/Header/Header";
+// import Nav from "./components/Nav/Nav";
+// import Footer from "./components/Footer/Footer";
 
 import Home from "./pages/Home";
 import Contacts from "./pages/Contacts";
