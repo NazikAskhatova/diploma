8e7cfebb7b904f0aebec6190add290e36ecef638
wiusxt <ashatovanas65@gmail.com>
2022-04-25 14:35:07 +0600

    added images

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -4,15 +4,63 @@ import Header from "../components/Header/Header";
 import ProductList from "../components/ProductList/ProductList";
 import image from "../assets/jew-shop.jpeg";
 
-function Products() {
-  const products = {};
-    return (
-      <>
-      <Header title="You can find whatever you want." image={image}>
-      </Header>
-   <ProductList products={products}/>
-    </>
-    );
-  }
+import Rings from "../assets/products-2.jpg";
+import Earrings from "../assets/products-3.jpg";
+import Bracellets from "../assets/products-5.jpg";
+import weddingRings from "../assets/products-6.jpg";
+import Necklakes from "../assets/products-7.jpg";
+import Watches from "../assets/products-card.jpg";
 
-  export default Products;
+function Products() {
+  const products = {
+   rings: {
+      image: Rings,
+      path: "/",
+      title: "All Catalog with Rings category is here",
+      price: 79.9,
+    },
+    whiteChocolateSalami: {
+      image: Earrings,
+      path: "/",
+      title: "All Catalog with Earrings category is here",
+      price: 89.9,
+    },
+    terrysChocolateCheesecake: {
+      image: Bracellets,
+      path: "/",
+      title: "All Catalog with Bracellets is here",
+      price: 119,
+    },
+    raspberryRoseMarshmallows: {
+      image: weddingRings,
+      path: "/",
+      title: "All Catalog with Wedding Rings is here",
+      price: 69,
+    },
+    whiteChocolateFudge: {
+      image: Necklakes,
+      path: "/",
+      title: "All Catalog with Necklakes is here",
+      price: 99.9,
+    },
+    strawberryCupcakes: {
+      image: Watches,
+      path: "/",
+      title: "All Catalog with Watches is here",
+      price: 109.9,
+    },
+  };
+
+  return (
+    <>
+      <Header
+        title="Shop with us unfortunaly"
+        image={image}>
+       
+      </Header>
+
+      <ProductList products={products} />
+    </>
+  );
+} 
+ export default Products;
