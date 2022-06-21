ec230ef7eccd52965ff1fe9c283c2b9846af8e6a
wiusxt <ashatovanas65@gmail.com>
2022-06-21 12:03:08 +0600

    added new nav images in assets and updated Nav.js

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -7,8 +7,7 @@ function Nav() {
   return (
       <ul className={classes.Nav}>
         <NavItem url="/">Home</NavItem>
-        <NavItem url="/products">Products</NavItem>
-        <NavItem url="/contacts">Contacts</NavItem>
+        <NavItem url="/products">Best Products</NavItem>
         <NavItem url="/about">About</NavItem>
       </ul>
   
