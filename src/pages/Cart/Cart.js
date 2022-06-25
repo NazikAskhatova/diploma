0f1031f5ad33aa3b5f886fe7dca60031d5969027
wiusxt <ashatovanas65@gmail.com>
2022-06-25 22:49:39 +0600

    added new components - CartDisplay, CartItem

--- src/pages/Cart/Cart.js
+++ src/pages/Cart/Cart.js
@@ -1,70 +1,92 @@
-import { useDispatch, useSelector } from "react-redux";
-import { getProducts } from "../../data/dataProducts";
+// import { useDispatch, useSelector } from "react-redux";
+// import { getProducts } from "../../data/dataProducts";
+// import { Link } from "react-router-dom";
+// import classes from "./Cart.module.css";
+// import { increment, decrement, remove } from "../../redux/cartSlice";
+// function Cart() {
+//   const items = useSelector((store) => store.cart.items);
+//   const dispatch = useDispatch();
+//   const products = getProducts();
+
+//   let total = 0;
+//   let output = products
+//     // let outputAmount = 0
+//     .filter((product) => items[product.productId])
+//     .map((product) => {
+//       total += product.price * items[product.productId];
+
+//       return (
+//         <>
+//           <div className={classes.Cart}>
+//             <Link to="">{product.title}</Link> {items[product.productId]} $
+//             {product.price * items[product.productId]}
+//             <div className={classes.QuantitySelector}>
+//               <button
+//                 className={classes.Current}
+//                 onClick={() => dispatch(decrement(product.productId))}
+//               >
+//                 -
+//               </button>
+//               <input className={classes.Current} />
+//               <button
+//                 className={classes.Current}
+//                 onClick={() => dispatch(increment(product.productId))}
+//               >
+//                 +
+//               </button>
+//             </div>
+//             <button
+//               className={classes.CartButton}
+//               onClick={() => dispatch(remove(product.productId))}
+//             >
+//               Delete
+//             </button>
+//             {/*        
+//         <button className={classes.anButton}></button> */}
+//           </div>
+//         </>
+//       );
+//     });
+
+//   if (!output) {
+//     output = "No items in the cart.";
+//   }
+
+//   return (
+//     <div className={classes.Cart}>
+//       <div>Products in your cart</div>
+//       <div>
+//         {output}
+//         {/* {outputAmount} */}
+//         Total: ${total}
+//         <Link className={classes.CartButton} to="/checkout">
+//           Checkout
+//         </Link>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default Cart;
+
+import Header from "../../components/Header/Header";
+import image from "../../assets/rings-product.webp"
+import CartDisplay from "../../components/CartDisplay/CartDispaly";
 import { Link } from "react-router-dom";
-import classes from "./Cart.module.css";
-import { increment, decrement, remove } from "../../redux/cartSlice";
+
 function Cart() {
-  const items = useSelector((store) => store.cart.items);
-  const dispatch = useDispatch();
-  const products = getProducts();
-
-  let total = 0;
-  let output = products
-    // let outputAmount = 0
-    .filter((product) => items[product.productId])
-    .map((product) => {
-      total += product.price * items[product.productId];
-
-      return (
-        <>
-          <div className={classes.Cart}>
-            <Link to="">{product.title}</Link> {items[product.productId]} $
-            {product.price * items[product.productId]}
-            <div className={classes.QuantitySelector}>
-              <button
-                className={classes.Current}
-                onClick={() => dispatch(decrement(product.productId))}
-              >
-                -
-              </button>
-              <input className={classes.Current} />
-              <button
-                className={classes.Current}
-                onClick={() => dispatch(increment(product.productId))}
-              >
-                +
-              </button>
-            </div>
-            <button
-              className={classes.CartButton}
-              onClick={() => dispatch(remove(product.productId))}
-            >
-              Delete
-            </button>
-            {/*        
-        <button className={classes.anButton}></button> */}
-          </div>
-        </>
-      );
-    });
-
-  if (!output) {
-    output = "No items in the cart.";
-  }
-
   return (
-    <div className={classes.Cart}>
-      <div>Products in your cart</div>
-      <div>
-        {output}
-        {/* {outputAmount} */}
-        Total: ${total}
-        <Link className={classes.CartButton} to="/checkout">
-          Checkout
-        </Link>
-      </div>
-    </div>
+    <>
+      <Header
+        title="Your Shopping Cart"
+        image={image}>
+        Please review items in your cart.
+      </Header>
+
+      <CartDisplay actions />
+
+      <Link to="/checkout">Checkout</Link>
+    </>
   );
 }
-
 export default Cart;
--- src/pages/CheckOut.js
+++ src/pages/CheckOut.js
@@ -1,74 +1,128 @@
-import { useDispatch, useSelector } from "react-redux";
-import { getProducts } from "../data/dataProducts";
-import { Link, useNavigate } from "react-router-dom";
-import { checkout } from "../redux/cartSlice";
-import React from "react";
+// import { useDispatch, useSelector } from "react-redux";
+// import { getProducts } from "../data/dataProducts";
+// import { Link, useNavigate } from "react-router-dom";
+// import { checkout } from "../redux/cartSlice";
+// import React from "react";
 
-function CheckOut() {
-  const items = useSelector(store => store.cart.items);
-  const products = getProducts();
+// function CheckOut() {
+//   const items = useSelector(store => store.cart.items);
+//   const products = getProducts();
+//   const dispatch = useDispatch();
+//   const navigate = useNavigate();
+//   let total = 0;
+//   let output = products
+//     .filter(product => items[product.productId])
+//     .map(product => {
+//       total += product.price * items[product.productId];
+
+//       return (
+//         <>
+//         <div>
+//           <Link to="">{product.title}</Link> {items[product.productId]} ${product.price * items[product.productId]}
+//         </div>
+//         </>
+//       );
+//     });
+
+//   if (!output) {
+//     output = "No items in the cart.";
+//   } 
+
+//   function onCheckout(event) {
+//     event.preventDefault();
+
+//     const formData = new FormData(event.target);
+//     const order = { items: items, ...Object.fromEntries(formData.entries()) };
+//     dispatch(checkout(order));
+//     navigate('/');
+//   }
+
+//   return (
+//     <div>
+//       <div>
+//         Please review items in your cart.
+//       </div>
+//       <div title="Checkout">
+//         {output}
+//         <hr />
+//         Total: ${total}
+
+//         <form onSubmit={onCheckout}>
+//           <label>
+//             First name:
+//             <input type="text" name="firstName" required />
+//           </label>
+//           <label>gy
+//             Last name:
+//             <input type="text" name="lastName" required />
+//           </label>
+//           <label>
+//             Address:
+//             <input type="text" name="address" required />
+//           </label>
+//           <label>
+//             Phone:
+//             <input type="text" name="phone" required />
+//           </label>
+//           <button>Complete the order</button>
+//         </form>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default CheckOut;
+
+import Header from "../components/Header/Header";
+import image from "../assets/rings-category.jpg";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { checkout } from "../redux/cartSlice";
+import CartDisplay from "../components/CartDisplay/CartDispaly";
+
+function Checkout() {
   const dispatch = useDispatch();
   const navigate = useNavigate();
-  let total = 0;
-  let output = products
-    .filter(product => items[product.productId])
-    .map(product => {
-      total += product.price * items[product.productId];
-
-      return (
-        <>
-        <div>
-          <Link to="">{product.title}</Link> {items[product.productId]} ${product.price * items[product.productId]}
-        </div>
-        </>
-      );
-    });
-
-  if (!output) {
-    output = "No items in the cart.";
-  } 
+  const items = useSelector(store => store.cart.items);
 
   function onCheckout(event) {
     event.preventDefault();
-
     const formData = new FormData(event.target);
     const order = { items: items, ...Object.fromEntries(formData.entries()) };
     dispatch(checkout(order));
     navigate('/');
   }
-
   return (
-    <div>
-      <div>
-        Please review items in your cart.
-      </div>
-      <div title="Checkout">
-        {output}
-        <hr />
-        Total: ${total}
+    <>
+      <Header
+        title="Checkout"
+        image={image}>
+        Please enter your information.
+      </Header>
 
-        <form onSubmit={onCheckout}>
-          <label>
-            First name:
-            <input type="text" name="firstName" required />
-          </label>
-          <label>
-            Last name:
-            <input type="text" name="lastName" required />
-          </label>
-          <label>
-            Address:
-            <input type="text" name="address" required />
-          </label>
-          <label>
-            Phone:
-            <input type="text" name="phone" required />
-          </label>
-          <button>Complete the order</button>
-        </form>
-      </div>
-    </div>
+      <CartDisplay />
+
+      <form onSubmit={onCheckout}>
+        <label>
+          First name:
+          <input type="text" name="firstName" required />
+        </label>
+        <label>
+          Last name:
+          <input type="text" name="lastName" required />
+        </label>
+        <label>
+          Address:
+          <input type="text" name="address" required />
+        </label>
+        <label>
+          Phone:
+          <input type="text" name="phone" required />
+        </label>
+
+        <button>Complete the order</button>
+      </form>
+    </>
   );
 }
-
-export default CheckOut;
+export default Checkout;
