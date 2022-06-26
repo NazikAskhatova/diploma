61aec699274133958f1b8a90318dd263782154e7
wiusxt <ashatovanas65@gmail.com>
2022-06-26 06:59:59 +0600

    added svg icons and using it in Nav.js file

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,18 +1,42 @@
 
 import React from "react";
+import Search from "../../assets/magnifying-glass-solid.svg";
+import UserIcon from "../../assets/user-solid.svg";
 import NavItem from "./NavItem/NavItem";
 import classes from "./Nav.module.css";
+import CartLink from "../CartLink/CartLink";
+import Logo from "../ui/Logo/Logo";
+import { NavLink } from "react-router-dom";
 
 function Nav() {
   return (
+    <div className={classes.Nav}>
+      
       <ul className={classes.Nav}>
-        <NavItem url="/">Home</NavItem>
-        <NavItem url="/products">Our Products</NavItem>
-        <NavItem url="/about">About</NavItem>
+         <NavItem url="/">Home</NavItem>
+      <NavItem url="/products">Our Products</NavItem>
+       <NavItem url="/about">About</NavItem>
       </ul>
-  
+      <NavLink to="/"><Logo /></NavLink>
+      
+        <NavLink to="/search" className={classes.icons}><img src={Search} className={classes.searchIcon} alt="Search" /></NavLink>
+        <NavLink to="/auth" className={classes.icons}><img src={UserIcon} className={classes.userIcon} alt="Account" /></NavLink>
+        <NavLink to="/cart" className={classes.icons}><CartLink /></NavLink>
+      
+    </div>
   );
 }
+
+// function Nav() {
+//   return (
+//       <ul className={classes.Nav}>
+//         <NavItem url="/">Home</NavItem>
+//         <NavItem url="/products">Our Products</NavItem>
+//         <NavItem url="/about">About</NavItem>
+//       </ul>
+//     <NavLink to="/" className={classes.logo}><Logo /></NavLink>
+//   );
+// }
 
 export default Nav;
 
--- src/components/Toolbar/Toolbar.js
+++ src/components/Toolbar/Toolbar.js
@@ -1,18 +1,14 @@
 import React from "react";
 import NavToggle from "../Nav/NavToggle/NavToggle";
-import Logo from "../ui/Logo/Logo";
 import classes from "./Toolbar.module.css";
-import CartLink from "../CartLink/CartLink";
 import Nav from "../Nav/Nav";
 
 function Toolbar({ toggleDrawer }) {
   return (
     <nav className={classes.Toolbar}>
       <div className={classes.container}>
-        <Logo />
         <Nav className="wrapper"/>
         <NavToggle callback={toggleDrawer} />
-        <CartLink />
       </div>
     </nav>
   );
