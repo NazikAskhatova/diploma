29afdaa93156c25c478ff838a34568951e6fadea
wiusxt <ashatovanas65@gmail.com>
2022-06-29 05:52:16 +0600

    added hover effect and link in ProductItem

--- src/components/ProductList/ProductItem/ProductItem.js
+++ src/components/ProductList/ProductItem/ProductItem.js
@@ -5,7 +5,10 @@ import classes from "./ProductItem.module.css";
 function ProductItem({ product }) {
   return (
     <div className={classes.ProductItem}>
-      <img src={product.image} alt={product.title} />
+       <Link to={"/products/" + product.productId}>
+         <img src={product.image} alt={product.title} /> 
+      </Link>
+     
       <Link to={"/products/" + product.productId} >{product.title}</Link>
       <div className={classes.price}>${product.price}</div>
     </div>
