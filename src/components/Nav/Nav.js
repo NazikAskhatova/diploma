316583530e14e2f3f7ec43de3c750af2499674e2
wiusxt <ashatovanas65@gmail.com>
2022-06-29 01:58:38 +0600

    added new svg icon, fixing the console problems

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -2,6 +2,7 @@
 import React from "react";
 import Search from "../../assets/magnifying-glass-solid.svg";
 import UserIcon from "../../assets/user-solid.svg";
+import Signout from "../../assets/right-from-bracket-solid.svg"
 import NavItem from "./NavItem/NavItem";
 import classes from "./Nav.module.css";
 import CartLink from "../CartLink/CartLink";
@@ -14,39 +15,82 @@ function Nav() {
   return (
     <div className={classes.Nav}>
       
-      <ul className={classes.Nav}>
+      <ul className={classes.List}>
          <NavItem url="/">Home</NavItem>
       <NavItem url="/products">all Products</NavItem>
       <NavItem url="/bestsellers">Best Sellers</NavItem>
        <NavItem url="/about">About</NavItem>
-       { isAuthenticated ? <NavItem url="/signout">Sign out</NavItem> : null }
-      { !isAuthenticated ? <NavItem url="/auth">Sign in</NavItem> : null }
       </ul>
-      <div><NavLink to="/"><Logo /></NavLink></div>
+
+      <div className={classes.Logo}
+      ><NavLink to="/"><Logo /></NavLink></div>
       <div className={classes.Nav}>
           <ul>
-        <NavLink to="/search" className={classes.icons}><img src={Search} className={classes.searchIcon} alt="Search" /></NavLink>
-        <NavLink to="/auth" className={classes.icons}><img src={UserIcon} className={classes.accountIcon} alt="Account" /></NavLink>
+          <NavLink to="/search" className={classes.icons}><img src={Search} className={classes.searchIcon} alt="Search" /></NavLink>
+      {!isAuthenticated ?
+          <NavLink to="/auth" className={classes.icons}>
+            <img src={UserIcon} className={classes.icons} alt="Account" />
+          </NavLink>
+        : null}
+        {isAuthenticated ? 
+          <NavLink to="/signout" className={classes.icons}>
+            <img src={Signout} className={classes.userIcon} alt="Account" />
+          </NavLink> 
+        : null}
         <NavLink to="/cart" className={classes.icons}><CartLink /></NavLink>
         </ul>
       </div>
     
     </div>
-  );
+);
+
 }
 
+// import classes from "./Nav.module.css";
+// import { useSelector } from "react-redux";
+// import Logo from "../ui/Logo";
+// import NavItem from "./NavItem/NavItem";
+// import React from "react";
+// import { NavLink } from "react-router-dom";
+// import Search from "../../assets/search.svg";
+// import User from "../../assets/userIcon.svg";
+// import signout from "../../assets/signout.svg";
+// import CartLink from "../CartLink/CartLink";
+
 // function Nav() {
+//   const isAuthenticated = useSelector(store => store.auth.idToken !== null);
+
 //   return (
-//       <ul className={classes.Nav}>
-//         <NavItem url="/">Home</NavItem>
-//         <NavItem url="/products">Our Products</NavItem>
-//         <NavItem url="/about">About</NavItem>
+//     <div className={classes.Nav}>
+//       <ul>
+//         <NavItem url="/shop">Shop</NavItem>
+//         <NavItem url="/specialdays">Special days</NavItem>
+//         <NavItem url="/location">Location</NavItem>
 //       </ul>
-//     <NavLink to="/" className={classes.logo}><Logo /></NavLink>
+//       <NavLink to="/" className={classes.logo}><Logo /></NavLink>
+//       <ul>
+//         <NavItem url="/about">About</NavItem>
+//         <NavItem url="/contacts">Contacts</NavItem>
+//         <NavLink to="/search" className={classes.icons}><img src={Search} className={classes.searchIcon} alt="Search" /></NavLink>
+//         {!isAuthenticated ?
+//           <NavLink to="/auth" className={classes.icons}>
+//             <img src={User} className={classes.userIcon} alt="Account" />
+//           </NavLink>
+//         : null}
+//         {isAuthenticated ? 
+//           <NavLink to="/signout" className={classes.icons}>
+//             <img src={signout} className={classes.userIcon} alt="Account" />
+//           </NavLink> 
+//         : null}
+//         <NavLink to="/cart" className={classes.icons}><CartLink /></NavLink>
+//       </ul>
+//     </div>
 //   );
 // }
 
 export default Nav;
+
+
 
 
 
--- src/components/Nav/NavItem/NavItem.js
+++ src/components/Nav/NavItem/NavItem.js
@@ -1,16 +1,15 @@
-import React from "react";
 import { NavLink, useMatch } from "react-router-dom";
 import classes from "./NavItem.module.css";
+import React from "react";
 
-function NavItem(props) {
+function NavItem({ url, active, children }) {
   return (
     <li className={classes.NavItem}>
-      <NavLink to={props.url}
-        className={useMatch(props.url) ? classes.active : null}
-      >
-        {props.children}
+      <NavLink to={url} className={useMatch(url) ? classes.active : null}> 
+        {children}
       </NavLink>
     </li>
   );
 }
+
 export default NavItem;
