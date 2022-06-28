c8339e0b92c3f28ed0d11fb15ac786f4bfb3c490
wiusxt <ashatovanas65@gmail.com>
2022-06-29 05:04:35 +0600

    added new page images and correcting code in css

--- src/components/ProductList/ProductItem/ProductItem.js
+++ src/components/ProductList/ProductItem/ProductItem.js
@@ -6,7 +6,7 @@ function ProductItem({ product }) {
   return (
     <div className={classes.ProductItem}>
       <img src={product.image} alt={product.title} />
-      <Link to={"/products/" + product.productId}>{product.title}</Link>
+      <Link to={"/products/" + product.productId} >{product.title}</Link>
       <div className={classes.price}>${product.price}</div>
     </div>
   );
