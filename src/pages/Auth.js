60efd905b90fa377ace8653693463b66ad366913
wiusxt <ashatovanas65@gmail.com>
2022-06-29 06:56:00 +0600

    added AuthDisplay, changed Nav, Auth was updated

--- src/pages/Auth.js
+++ src/pages/Auth.js
@@ -1,35 +1,10 @@
 import React from "react";
-import { useDispatch } from "react-redux";
-import Header from "../components/Header/Header";
-import { start } from "../redux/authSlice";
+import AuthDisplay from "../components/AuthDisplay/AuthDisplay";
 
 export default function Auth() {
-  const dispatch = useDispatch();
-
-  function onAuthStart(event) {
-    event.preventDefault();
-
-    const formData = new FormData(event.target);
-    dispatch(start({
-      email: formData.get('email'),
-      password: formData.get('password'),
-      method: 'signup'
-    }));
-  }
-
   return (
-    <form onSubmit={onAuthStart}>
-        <Header></Header>
-      <label>
-        Email:
-        <input type="email" name="email" />
-      </label>
-      <label>
-        Password:
-        <input type="password" name="password" />
-      </label>
-      <button>Sign in</button>
-      <button>Sign up</button>
-    </form>
+    <>
+      <AuthDisplay />
+    </>
   );
 }
