5d834f8b83bc3e2504c77044ab9b0cd09116392a
wiusxt <ashatovanas65@gmail.com>
2022-06-30 02:24:47 +0600

    added modboard img, styling HomeElement component

--- src/components/HomeElement/HomeElement.js
+++ src/components/HomeElement/HomeElement.js
@@ -1,11 +1,14 @@
 import React from "react";
 import classes from "./HomeElement.module.css";
+import moodboardEr from "../../assets/Moodboard-Image-Earring.webp"
+import Homeline from "../../assets/home-lineup.webp"
 function HomeEl() {
   return (
     <>
       <div className={classes.Homeart}>
-        <article>
-          <h3>Handcrafted and Responsibly Sourced</h3>
+          
+          <div className={classes.container}>
+            <h3>Handcrafted and Responsibly Sourced</h3>
           <h5>FAIR PRICING </h5>
           <p>
             Say goodbye to traditional 10x markups. We design fine jewelry that
@@ -16,8 +19,11 @@ function HomeEl() {
             We work with expert jewelers who use high-quality and enduring
             materials, from precious metals—such as recycled and responsibly
             mined gold—to responsibly sourced diamonds and AAA-grade gemstones.
-          </p>
-        </article>
+          </p>   
+          <button>Learn More</button>
+          </div>
+        <img src={Homeline} alt="HomeLine" className={classes.brings}/>
+        <img src={moodboardEr} alt="earmood" className={classes.moodboard}/>
       </div>
     </>
   );
