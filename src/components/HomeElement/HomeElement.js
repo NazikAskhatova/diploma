36f7eb503b2083984f9746f09551ec7484616085
wiusxt <ashatovanas65@gmail.com>
2022-06-30 00:47:53 +0600

    added new png image file for NavToggle icon

--- src/components/HomeElement/HomeElement.js
+++ src/components/HomeElement/HomeElement.js
@@ -5,13 +5,13 @@ function HomeEl() {
     <>
       <div className={classes.Homeart}>
         <article>
-          <h3> Handcrafted and Responsibly Sourced</h3>
-          <h5> FAIR PRICING </h5>
+          <h3>Handcrafted and Responsibly Sourced</h3>
+          <h5>FAIR PRICING </h5>
           <p>
             Say goodbye to traditional 10x markups. We design fine jewelry that
             you can wear every single day.
           </p>
-          <h5> HIGH QUALITY</h5>
+          <h5>HIGH QUALITY</h5>
           <p>
             We work with expert jewelers who use high-quality and enduring
             materials, from precious metals—such as recycled and responsibly
