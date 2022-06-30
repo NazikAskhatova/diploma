633c6c60d4152a62b30c7130c2fe7beb0134bc46
wiusxt <ashatovanas65@gmail.com>
2022-06-30 13:58:39 +0600

    added new styles AboutUs.css

--- src/components/AboutUs/AboutUs.js
+++ src/components/AboutUs/AboutUs.js
@@ -2,10 +2,10 @@ import React from "react";
 import "./AboutUs.module.css";
 function AboutUs() {
   return (
-    <>
+    <div className="fixed">
     <div className="shopby">About Us</div>
     <hr className="hr-st"></hr>
-    </>
+    </div>
   ) 
 }
 
